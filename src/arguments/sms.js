467465a304ffa6fafdbe356d4cfc619ff5e9a284
Kevin Lewis <kvn@lws.io>
2019-10-18 01:22:47 +0100

    Refactored sms argument around new setup command

--- src/arguments/sms.js
+++ src/arguments/sms.js
@@ -1,57 +1,55 @@
+// Syntax nexmonaut sms <message>
+// If there's no application ID or link to private key - ask and store. 
+// If there is - go straight to asking to:from
+// const privateKey = fs.readFileSync(absKeyLocation)
+
 const { Command } = require('@adonisjs/ace')
 const Nexmo = require('nexmo')
 const fs = require('fs')
+const username = require('os').userInfo().username
+
 
 class Sms extends Command {
   static get signature() {
-    return `sms`
+    return `sms {message: The message you want to send}`
   }
 
   static get description() {
     return `Send a SMS using Nexmo`
   }
 
-  async handle(args, flags) {
-    // Grab environment variables if they exist
-    const { NEXMO_API_KEY, NEXMO_API_SECRET } = process.env
-    let apiKey, apiSecret;
+  async handle({ message }, flags) {
+    const configFile = `/users/${username}/.nexmo`
 
-    // If either the key or secret doesn't exist, prompt the user for them
-    if(!NEXMO_API_KEY) API_KEY = await this.ask(`What's your Nexmo API Key?`)
-    else apiKey = NEXMO_API_KEY
-    if(!NEXMO_API_SECRET) API_SECRET = await this.ask(`What's your Nexmo API Secret?`)
-    else apiSecret = NEXMO_API_SECRET
-
-    const applicationId = await this.ask(`What's your application ID?`)
-    const privateKeyLocation = await this.ask(`Where's the location of your private.key file?`, `/nexmo`)
-    const privateKey = fs.readFileSync(`${process.cwd()}${privateKeyLocation}/private.key`)
-
-    // Final check - did user enter required data. If not, provide error and break
-    if(!apiKey || !apiSecret || !applicationId || !privateKey) {
-      this.error(`Can't make request as information is missing`)
-      return
-    }
-
-    // Get user input to send message
-    const from = await this.ask(`From number`, `NEXMO`)
-    const to = await this.ask(`Recipient number`)
-    const message = await this.ask(`What's your message?`)
-    
-    const nexmo = new Nexmo({ apiKey, apiSecret, applicationId, privateKey });
-
-    // Send message!
-    nexmo.channel.send(
-      { type: `sms`, number: to },
-      { type: `sms`, number: from },
-      { content: { type: `text`, text: message } },
-      (err, data) => {
-        if(err) {
-          console.log(err)
+    if(fs.existsSync(configFile)) {
+      fs.readFile(configFile, `utf8`, async (err, data) => {
+        if (err) {
+          this.error(`Error reading config. Please run nexmonaut setup.`)
           return
         }
-        this.success(`Sent message!`)
-      }
-    )
+        const { apiKey, apiSecret, applicationId, privateKey: pk } = JSON.parse(data)
+
+        const privateKey = fs.readFileSync(pk)
+        const nexmo = new Nexmo({ apiKey, apiSecret, applicationId, privateKey })
+        const from = await this.ask(`From number`, `NEXMO`)
+        const to = await this.ask(`Recipient number`)
+
+        nexmo.channel.send(
+          { type: `sms`, number: to },
+          { type: `sms`, number: from },
+          { content: { type: `text`, text: message } },
+          (err, data) => {
+            if(err) {
+              console.log(err)
+              return
+            }
+            this.success(`Sent message!`)
+          }
+        )
+      })
+    } else {
+      this.error('Please run nexmonaut setup first')
+    }
   }
 }
 
